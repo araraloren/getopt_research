64379c40a6f78a883b1d75e2fc5a190b9c86553a
loren <blackcatoverwall@gmail.com>
2021-04-17 19:52:01 +0800

    Update proc.rs

--- src/proc.rs
+++ src/proc.rs
@@ -7,23 +7,19 @@ use std::fmt::Debug;
 pub trait Message: Debug {
     type Data;
 
-    fn msg_id(&self) -> u64;
+    fn id(&self) -> u64;
 
     fn data(&mut self) -> &mut Self::Data;
 }
 
-pub trait Info<M: Message>: Debug {
-    fn info_id(&self) -> u64;
-
-    fn check(&self, msg: &M) -> bool;
-
-    fn process(&mut self, data: &mut M::Data, opt: &mut dyn Opt);
+pub trait Info: Debug {
+    fn id(&self) -> u64;
 }
 
 pub trait Publisher<M: Message> {
     fn publish(&mut self, msg: M);
 
-    fn subscribe(&mut self, info: Box<dyn Info<M>>);
+    fn subscribe(&mut self, info: Box<dyn Info>);
 
     fn clean(&mut self);
 }
@@ -33,18 +29,14 @@ pub struct Proc {
     // id of current Proc
     proc_id: u64,
 
-    // current option style 
-    style: Style,
-
     // context need process
     ctxs: Vec<Box<dyn Context<Proc>>>,
 }
 
 impl Proc {
-    pub fn new(id: u64, style: Style) -> Self {
+    pub fn new(id: u64) -> Self {
         Self {
             proc_id: id,
-            style,
             ctxs: vec![],
         }
     }
@@ -53,15 +45,27 @@ impl Proc {
         self.ctxs.push(ctx);
     }
 
-    pub fn run(&mut self, opt: &mut dyn Opt) {
+    pub fn check(&self, opt: &dyn Opt) -> bool {
+        for ctx in &self.ctxs {
+            if ctx.match_msg(self, opt) {
 
+            }
+            else {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn run(&mut self, opt: &mut dyn Opt) {
+        debug!("");
     }
 }
 
 impl Message for Proc {
     type Data = Proc;
 
-    fn msg_id(&self) -> u64 {
+    fn id(&self) -> u64 {
         self.proc_id
     }
 
