d8ad1f41cda7ef1c1c13187a11389a917df1c4e7
loren <blackcatoverwall@gmail.com>
2021-05-09 16:50:55 +0800

    Update ctx.rs

--- getopt-rs/src/ctx.rs
+++ getopt-rs/src/ctx.rs
@@ -88,23 +88,25 @@ impl Context for OptContext {
             opt.match_style(self.style.clone()) &&
             ((opt.match_name(self.opt_name.as_str()) && opt.match_prefix(self.opt_prefix.as_str()))
                 || opt.match_alias(&self.opt_prefix, &self.opt_name));
-        debug!("Match Opt<{:?}> {:?}: {}", opt.id(), opt, matched);
+        debug!("Match Opt<{:?}> {:?} => ", opt.id(), opt);
+        debug!(">>** {}", if matched { "TRUE" } else { "FALSE" });
         matched
     }
 
     fn process(&mut self, opt: &mut dyn Opt) -> Result<bool> {
-        self.matched = true;
+        let mut value = &String::default();
+        
         debug!("Match successed => {:?} : Opt<{:?}>", self, opt.id());
+        self.matched = true;
         if opt.is_need_argument() && self.next_argument.is_none() {
             return Err(Error::ArgumentRequired(format!("{}{}", opt.prefix(), opt.name())));
         }
-        let mut value = &String::default();
-
         if let Some(v) = &self.next_argument {
             value = v;
         }
-
+        // we always call `set_value` since all option type need update value through this function
         opt.set_value(opt.parse_value(value.as_str())?);
+        opt.set_need_invoke(true);
         Ok(true)
     }
 
@@ -114,5 +116,80 @@ impl Context for OptContext {
 
     fn is_need_argument(&self) -> bool {
         self.skip_next_arg
+    }
+}
+
+#[derive(Debug)]
+pub struct NonOptContext {
+    opt_name: String,
+
+    style: Style,
+
+    total: i64,
+
+    current: i64,
+
+    matched: bool,
+}
+
+impl NonOptContext {
+    pub fn new(opt_name: String, style: Style, total: i64, current: i64) -> Self {
+        Self {
+            opt_name,
+            style,
+            total,
+            current,
+            matched: false,
+        }
+    }
+
+    pub fn set_name(&mut self, name: String) -> &mut Self {
+        self.opt_name = name;
+        self
+    }
+
+    pub fn set_style(&mut self, style: Style) -> &mut Self {
+        self.style = style;
+        self
+    }
+
+    pub fn set_total(&mut self, total: i64) -> &mut Self {
+        self.total = total;
+        self
+    }
+
+    pub fn set_current(&mut self, current: i64) -> &mut Self {
+        self.current = current;
+        self
+    }
+}
+
+impl Context for NonOptContext {
+    fn match_opt(&self, opt: &dyn Opt) -> bool {
+        let matched = 
+            opt.match_style(self.style.clone()) && 
+            opt.match_index(self.total, self.current) &&
+            opt.match_name(&self.opt_name);
+
+        debug!("Match NonOpt<{:?}> {:?} => ", opt.id(), opt);
+        debug!(">>^^ {}", if matched { "TRUE" } else { "FALSE" });
+        matched
+    }
+
+    fn process(&mut self, opt: &mut dyn Opt) -> Result<bool> { 
+        debug!("Match successed => {:?} : Opt<{:?}>", self, opt.id());
+        self.matched = true;
+        // opt_name will be ignored
+        opt.set_value(opt.parse_value(&self.opt_name)?);
+        opt.set_need_invoke(true);
+        Ok(true)
+    }
+
+    fn is_matched(&self) -> bool {
+        self.matched
+    }
+
+    fn is_need_argument(&self) -> bool {
+        false
     }
 }
